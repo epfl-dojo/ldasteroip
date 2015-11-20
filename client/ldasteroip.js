ada940bb0b79dbeb268308602fa4a8f9d41806e9
Dominique Quatravaux <dominique@quatravaux.org>
2015-11-20 14:03:39 +0100

    Persistent SCIPER list!!1!
    * Server method getUserBySciper, apparently queried over WebSocket
    * Client updates session
    * Session updates templates
    * Magic makes it persistent

--- client/ldasteroip.js
+++ client/ldasteroip.js
@@ -1,9 +1,12 @@
-
+Meteor.subscribe("UserList");
 Session.setDefault('counter', 0);
 
 Template.hello.helpers({
   counter: function () {
     return Session.get('counter');
+  },
+  users: function () {
+    return Session.get('userList');
   }
 });
 
@@ -11,8 +14,21 @@ Template.hello.events({
   'click button': function () {
     // increment the counter when button is clicked
     Session.set('counter', Session.get('counter') + 1);
+    var sciper = document.getElementById("sciper").value;
 
-    Meteor.call('getUserBySciper', 150938);
+    Meteor.call('getUserBySciper', sciper, function(err, response) {
+      if(err) {
+        alert('ERROR: ' + err);
+      } else {
+        var newUserList = Session.get('userList');
+        if (newUserList === undefined) {
+          newUserList = [];
+        }
+        newUserList.push(response);
+        Session.set('userList', newUserList);
+        document.getElementById("response").innerHTML = JSON.stringify(response);
+      }
+    });
   }
 });
 
--- server/backend.js
+++ server/backend.js
@@ -1,13 +1,34 @@
 Meteor.startup(function () {
     // code to run on server at startup
+
+    var ldapContext = Meteor.npmRequire('epfl-ldap')();
+     var getSyncUserBySciper = Meteor.wrapAsync(ldapContext.users.getUserBySciper);
+     var result = getSyncUserBySciper('133134');
+    //var result = {};
+
+    console.log(result);
+
+    UserList.insert(result);
 });
 
 Meteor.methods({
    getUserBySciper: function(sciper){
-       var ldapContext = Meteor.npmRequire('epfl-ldap');
+       var ldapContext = Meteor.npmRequire('epfl-ldap')();
        var getSyncUserBySciper = Meteor.wrapAsync(ldapContext.users.getUserBySciper);
        var result = getSyncUserBySciper(sciper);
        //insert result in db
        return result;
-   }
+   },
+    getCurrentTime: function (stuff) {
+        console.log(stuff);
+        console.log('on server, getCurrentTime called');
+        return new Date();
+    }
+});
+
+Meteor.publish("UserList", function () {
+
+
+        return UserList.find();
+
 });
